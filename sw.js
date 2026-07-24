150b0b802262e1d9c2d9cf65d3feb55ce6cd8bee
Amanda Haasbroek <amandah@discovery.co.za>
2026-07-24 22:57:38 +0200

    v13

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  *   - Use "stale-while-revalidate" for the main HTML so users get
  *     the newest version on refresh but nothing ever fails offline
  */
-const CACHE_VERSION = 'cricket-scorer-v12.1';
+const CACHE_VERSION = 'cricket-scorer-v13';
 const APP_SHELL = [
   './',
   './index.html',
